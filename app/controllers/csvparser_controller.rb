ec57d2b26fd2ca5a7a9619f68fcaf709f91f5186
Isaac Keith <iceykohnn@gmail.com>
2015-02-07 13:17:36 -0600

    working on parser

--- app/controllers/csvparser_controller.rb
+++ app/controllers/csvparser_controller.rb
@@ -1,4 +1,10 @@
 class CsvParserController < ApplicationController
+	
+	def index
+		@csv_parsers = CsvParser.all
+	end
+
+
 	def import
     CsvParser.import(params[:file])
     redirect_to root_url, notice: "File imported."
